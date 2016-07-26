a615996a0ec7731ae36f9914ac7be1d610de2306
Rainer Schleevoigt <rs@hamburger-appwerft.de>
2016-07-26 17:47:36 +0200

          

--- android/src/de/appwerft/audiocontrols/AudiocontrolsModule.java
+++ android/src/de/appwerft/audiocontrols/AudiocontrolsModule.java
@@ -7,15 +7,27 @@ import org.appcelerator.kroll.common.Log;
 import org.appcelerator.titanium.TiApplication;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Build;
 import android.os.ResultReceiver;
+import android.support.v4.app.NotificationCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.RemoteViews;
 
 @Kroll.module(name = "Audiocontrols", id = "de.appwerft.audiocontrols")
 public class AudiocontrolsModule extends KrollModule {
 	Context context;
 	public static String rootActivityClassName = "";
 	final String LCAT = "LockAudioScreen ♛♛♛";
+	final int NOTIFICATION_ID = 1;
 	static ResultReceiver resultReceiver;
 	Intent lockscreenService;
 
@@ -35,8 +47,30 @@ public class AudiocontrolsModule extends KrollModule {
 
 	}
 
+	private RemoteViews getAudioControlView() {
+		// Using RemoteViews to bind custom layouts into Notification
+		int layoutId = context.getResources().getIdentifier(
+				"remoteaudiocontrol", "layout", context.getPackageName());
+		Log.d(LCAT, "layoutId = " + layoutId);
+		if (layoutId == 0) {
+			return null;
+		}
+		RemoteViews customenotificationView = new RemoteViews(
+				context.getPackageName(), layoutId);
+
+		// Locate and set the Image into customnotificationtext.xml ImageViews
+		// notificationView.setImageViewResource(R.id.imagenotileft,
+		// R.drawable.ic_launcher);
+
+		// Locate and set the Text into customnotificationtext.xml TextViews
+		// notificationView.setTextViewText(R.id.title, getTitle());
+		// notificationView.setTextViewText(R.id.text, getText());
+
+		return customenotificationView;
+	}
+
 	@Kroll.method
-	public void createLockScreenControl(KrollDict opts) {
+	public void createRemoteAudioControl(KrollDict opts) {
 		context = TiApplication.getInstance().getApplicationContext();
 		String title = "", artist = "", image = "";
 		if (opts != null && opts.containsKeyAndNotNull("title")) {
@@ -48,35 +82,92 @@ public class AudiocontrolsModule extends KrollModule {
 		if (opts != null && opts.containsKeyAndNotNull("image")) {
 			image = opts.getString("image");
 		}
-		try {
-			// lockscreenService = new Intent(
-			// TiApplication.getAppCurrentActivity(),
-			// LockScreenService.class);
+		Resources res = context.getResources();
+		String pn = context.getPackageName();
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+			// Vorbild:
+			// http://stackoverflow.com/questions/23222063/android-custom-notification-layout-with-remoteviews
+			// http://www.laurivan.com/android-notifications-with-custom-layout/
+			Log.d(LCAT, " >= Build.VERSION_CODES.LOLLIPOP");
+			Intent intent = new Intent(context, LockScreenService.class);
+			PendingIntent contentIntent = PendingIntent.getActivity(context, 0,
+					intent, PendingIntent.FLAG_UPDATE_CURRENT);
+			int iconId = res.getIdentifier("notification_icon", "drawable", pn);
+			NotificationCompat.Builder builder = new NotificationCompat.Builder(
+					context).setSmallIcon(iconId)
+					.setContentIntent(contentIntent).setContentText("")
+					.setAutoCancel(false);
 
-			Intent lockscreenService = new Intent();
-			String pn = context.getPackageName();
-			lockscreenService.setAction(pn + ".LockScreenService");
-			lockscreenService.setPackage(pn);
+			NotificationManager notificationManager = (NotificationManager) context
+					.getSystemService(Context.NOTIFICATION_SERVICE);
 
-			lockscreenService.putExtra("title", title);
-			lockscreenService.putExtra("artist", artist);
-			lockscreenService.putExtra("image", image);
-			/* for back communication */
-			// lockscreenService.putExtra("receiver", resultReceiver);
-			context.startService(lockscreenService);
-			Log.d(LCAT,
-					pn + ".LockScreenService try to start with "
-							+ opts.toString());
-		} catch (Exception ex) {
-			Log.d(LCAT, "Exception caught:" + ex);
+			Notification notification;
+
+			notification = builder.build();
+
+			// important: call this after building
+			// (http://stackoverflow.com/questions/21237495/create-custom-big-notifications)
+			notification.bigContentView = getAudioControlView();
+
+			// for making sticky
+			notification.flags |= Notification.FLAG_NO_CLEAR;
+
+			notificationManager.notify(NOTIFICATION_ID, notification);
+
+			LayoutInflater inflater = (LayoutInflater) context
+					.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+			Button playButton = (Button) inflater.inflate(
+					res.getIdentifier("playcontrol", "layout", pn), null);
+
+			playButton.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					Log.d(LCAT, ">>>>>>>>>><<<<<<<");
+				}
+			});
+
+		} else {
+			// for preL we open a view over lockscreen:
+			Log.d(LCAT, " < Build.VERSION_CODES.LOLLIPOP");
+			try {
+				Intent lockscreenService = new Intent();
+				lockscreenService.setAction(pn + ".LockScreenService");
+				lockscreenService.setPackage(pn);
+				lockscreenService.putExtra("title", title);
+				lockscreenService.putExtra("artist", artist);
+				lockscreenService.putExtra("image", image);
+				/* for back communication */
+				// lockscreenService.putExtra("receiver", resultReceiver);
+				context.startService(lockscreenService);
+				Log.d(LCAT,
+						pn + ".LockScreenService try to start with "
+								+ opts.toString());
+			} catch (Exception ex) {
+				Log.d(LCAT, "Exception caught:" + ex);
+			}
 		}
+	}
+
+	public void onStartStop(View view) {
+		Log.d(LCAT, "▶︎◼︎ ▶︎◼ ︎▶︎◼︎");
+
+	}
+
+	public void onForward(View view) {
+		Log.d(LCAT, ">>>>>>>>>>>>>>>>>>>");
+
+	}
+
+	public void onRewind(View view) {
+		Log.d(LCAT, "<<<<<<<<<<<<<<<<<<<<");
 	}
 
 	@Kroll.method
 	public void setContent(KrollDict opts) {
 		// http://stackoverflow.com/questions/15346647/android-passing-variables-to-an-already-running-service
 		// Alias to create…
-		this.createLockScreenControl(opts);
+		this.createRemoteAudioControl(opts);
 	}
 
 	@Override
--- android/src/de/appwerft/audiocontrols/LockScreenService.java
+++ android/src/de/appwerft/audiocontrols/LockScreenService.java
@@ -30,88 +30,83 @@ public class LockScreenService extends Service {
 
 	public LockScreenService() {
 		super();
+		context = TiApplication.getInstance().getApplicationContext();
 		Log.d(LCAT, "CONSTRUCTOR	");
 	}
 
 	@Override
 	public void onCreate() {
 		Log.d(LCAT, "onCreate");
-		windowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+		windowManager = (WindowManager) context
+				.getSystemService(WINDOW_SERVICE);
+		Log.d(LCAT, "inside service on start of onStartCommand");
+
+		Resources res = context.getResources();
+		String pn = context.getPackageName();
+		LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+
+		// we get a context from app for getting the view from XML:
+		int layoutId = context.getResources().getIdentifier(
+				"remoteaudiocontroler", "layout", pn);
+		audiocontrolView = (View) inflater.inflate(layoutId, null);
+
+		// getting references to buttons:
+		final int rewindcontrolId, forwardcontrolId, playcontrolId;
+		Button rewindButton = (Button) inflater.inflate(
+				rewindcontrolId = res.getIdentifier("rewindcontrol", "layout",
+						pn), null);
+		Button forwardButton = (Button) inflater.inflate(
+				forwardcontrolId = res.getIdentifier("rewindcontrol", "layout",
+						pn), null);
+		Button playButton = (Button) inflater.inflate(
+				playcontrolId = res.getIdentifier("playcontrol", "layout", pn),
+				null);
+		OnClickListener buttonListener = new View.OnClickListener() {
+			@Override
+			public void onClick(View clicksource) {
+				int buttonId = clicksource.getId();
+				String msg = "";
+				if (buttonId == rewindcontrolId)
+					msg = "rewind";
+				if (buttonId == forwardcontrolId)
+					msg = "forward";
+				if (buttonId == playcontrolId)
+					msg = "play";
+				Bundle bundle = new Bundle();
+				bundle.putString("lockscreen", msg);
+				resultReceiver.send(100, bundle);
+			}
+		};
+		rewindButton.setOnClickListener(buttonListener);
+		forwardButton.setOnClickListener(buttonListener);
+		playButton.setOnClickListener(buttonListener);
+
+		// http://stackoverflow.com/questions/19846541/what-is-windowmanager-in-android
+		// adding to window stack:
+		layoutParams = new WindowManager.LayoutParams(
+				WindowManager.LayoutParams.FILL_PARENT, 50,
+				// This allows the view to be displayed over the status bar
+				// WindowManager.LayoutParams.TYPE_SYSTEM_ALERT |
+				WindowManager.LayoutParams.TYPE_SYSTEM_OVERLAY,
+				// this is to keep button presses going to the background
+				// window
+				WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
+						| WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH
+						| WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
+						| WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+
+				PixelFormat.TRANSPARENT);
+		layoutParams.gravity = Gravity.TOP;
+		layoutParams.y = 50;
+		audiocontrolView.setBackgroundColor(0xffff0000);
+
 	}
 
 	@SuppressWarnings("deprecation")
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
-		Log.d(LCAT, "inside service on start of onStartCommand");
-		/* for back communication */
 		resultReceiver = intent.getParcelableExtra("receiver");
-		context = TiApplication.getInstance().getApplicationContext();
-		Resources res = context.getResources();
-		String pn = context.getPackageName();
-		LayoutInflater inflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
-		if (audiocontrolView == null) {
-			// we get a context from app for getting the view from XML:
-			int layoutId = context.getResources().getIdentifier("content_main",
-					"layout", pn);
-			audiocontrolView = (View) inflater.inflate(layoutId, null);
-
-			// getting references to buttons:
-			final int rewindcontrolId, forwardcontrolId, playcontrolId;
-			Button rewindButton = (Button) inflater.inflate(
-					rewindcontrolId = res.getIdentifier("rewindcontrol",
-							"layout", pn), null);
-			Button forwardButton = (Button) inflater.inflate(
-					forwardcontrolId = res.getIdentifier("rewindcontrol",
-							"layout", pn), null);
-			Button playButton = (Button) inflater.inflate(
-					playcontrolId = res.getIdentifier("rewindcontrol",
-							"layout", pn), null);
-			OnClickListener buttonListener = new View.OnClickListener() {
-				@Override
-				public void onClick(View clicksource) {
-					int buttonId = clicksource.getId();
-					String msg = "";
-					if (buttonId == rewindcontrolId)
-						msg = "rewind";
-					if (buttonId == forwardcontrolId)
-						msg = "forward";
-					if (buttonId == playcontrolId)
-						msg = "play";
-					Bundle bundle = new Bundle();
-					bundle.putString("lockscreen", msg);
-					resultReceiver.send(100, bundle);
-				}
-			};
-			rewindButton.setOnClickListener(buttonListener);
-			forwardButton.setOnClickListener(buttonListener);
-			playButton.setOnClickListener(buttonListener);
-
-			// http://stackoverflow.com/questions/19846541/what-is-windowmanager-in-android
-			// adding to window stack:
-			layoutParams = new WindowManager.LayoutParams(
-					WindowManager.LayoutParams.FILL_PARENT, 50,
-					// This allows the view to be displayed over the status bar
-					// WindowManager.LayoutParams.TYPE_SYSTEM_ALERT |
-					WindowManager.LayoutParams.TYPE_SYSTEM_OVERLAY,
-					// this is to keep button presses going to the background
-					// window
-					WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE
-							| WindowManager.LayoutParams.FLAG_WATCH_OUTSIDE_TOUCH
-							| WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL
-							| WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
-
-					PixelFormat.TRANSPARENT);
-			layoutParams.gravity = Gravity.TOP;
-			layoutParams.y = 50;
-			audiocontrolView.setBackgroundColor(0xffff0000);
-			// windowManager.addView(audiocontrolView, layoutParams);
-
-			// If we get killed, after returning from here, restart
-		} else {
-			// Update view
-
-		}
-		return START_REDELIVER_INTENT;
+		return START_STICKY;
 	}
 
 	@Override
@@ -147,3 +142,5 @@ public class LockScreenService extends Service {
 	}
 
 }
+
+//	
